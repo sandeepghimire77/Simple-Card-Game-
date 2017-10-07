a25e954fd7b3c90933b855d6ff6ec3a4a8728140
sandeepghimire <sandeep.ghimire@student.fh-kiel.de>
2017-10-07 14:45:59 +0200

    2nd solution of passing multiple comprehensions of list ..cool solution

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,14 +3,14 @@ defmodule Cards do
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-cards = for values <- values do
-    for suit <- suits do
-      "#{values} of #{suit}"
+     for suit <- suits, value <- values do
+
+      "#{value} of #{suit}"
     end
 
-    end
 
-    List.flatten(cards)
+
+
 
 
 
