6c6499aeca901a7183e248ef0b1351388e62b504
sandeepghimire <sandeep.ghimire@student.fh-kiel.de>
2017-10-07 14:43:09 +0200

    comprehensions lists .. 1st solution using list.flatten built in method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,11 +1,30 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+cards = for values <- values do
+    for suit <- suits do
+      "#{values} of #{suit}"
+    end
+
+    end
+
+    List.flatten(cards)
+
+
+
   end
   def  shuffle(deck) do
     deck = Cards.create_deck
     Enum.shuffle(deck)
+  end
+
+    def contains?(deck,card) do
+      Enum.member?(deck,card)
+
+
+
 
 
   end
