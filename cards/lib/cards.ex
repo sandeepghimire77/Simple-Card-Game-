cab476af614f5c3c2684381271dfeab13d81ad19
sandeepghimire <sandeep.ghimire@student.fh-kiel.de>
2017-10-08 00:03:23 +0200

    load a backup with cool error handling

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -27,6 +27,16 @@ def save(deck, filename) do
   File.write(filename,binary)
 end
 
+def  load(filename) do
+  {status, binary} = File.read(filename)
+  case status do
+    :ok -> :erlang.binary_to_term binary
+    :error -> "that file does not exist anymore"
+  end
+
+
+end
+
 
 
 
